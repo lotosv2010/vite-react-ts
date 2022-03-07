c5af99b95dc07c5fc52ea9a1c8d547a129b75afb
gaowenbin <lotosv2010@163.com>
2022-03-07 22:45:16 +0800

    feat(vite-plugin-html): 压缩 Html

--- config/plugins/index.ts
+++ config/plugins/index.ts
@@ -1,9 +1,10 @@
 import configEslint from './eslint';
 import { VITE_APP_VISUALIZER } from '../index';
 import configVisualizerConfig from './visualizer';
+import configHtml from './html';
 
 export default function createVitePlugins() {
-  const vitePlugins: any[] = [configEslint()];
+  const vitePlugins: any[] = [configEslint(), configHtml()];
   VITE_APP_VISUALIZER && vitePlugins.push(configVisualizerConfig());
   return vitePlugins;
 }
