45fc5c952d745c2df1deec41ef0591cb2845589c
gaowenbin <lotosv2010@163.com>
2022-04-02 17:12:15 +0800

    feat(script): 跟新脚本

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'mobx-react';
 import App from '@/App';
 import stores from '@/stores';
-
 import '@/index.scss';
-
+// App 入口
 ReactDOM.render(
   <Provider {...stores}>
     <App />
